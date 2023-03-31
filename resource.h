46ba1928aed4654c61975401aa2fa289d5c2424f
rtl-ylnomoc <lokate52@gmail.com>
2023-03-31 12:27:17 +0500

    fixing partly not working icon

--- resource.h
+++ resource.h
@@ -2,15 +2,15 @@
 // Microsoft Visual C++ generated include file.
 // Used by GamepadAsPen.rc
 //
-#define IDI_ICON2                       102
-#define IDI_ICON3                       103
-#define IDI_ICON1                       106
+#define IDI_ICON1                       107
+#define IDI_ICON2                       108
+#define IDI_ICON3                       109
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
